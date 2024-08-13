948bd4513dc17f889720ea444f687316b5f9b683
Emiliano Cenizo <emicenizo@gmail.com>
2024-08-13 14:39:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Bienvenido a mi sitio web</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Este es un sitio web de una sola página creado con React y alojado en GitHub Pages.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
